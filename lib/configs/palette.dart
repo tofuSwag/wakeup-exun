17f501548b2a90217c097fdb5d0f8f07b3c173b9
parzuko <jivansh17@gmail.com>
2020-11-17 02:09:04 +0530

    add palette

---
+++ lib/configs/palette.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  
+  // Theme Colors
+  static const Color scaffold = Color(0xffFFFAF2);
+  static const Color textColor = Color(0xff181819);
+  static const Color accent = Color(0xffFF9062);
+  static const Color hint = Color(0xff84B7B6);
+  static const Color lightHint = Color(0xffDAEFEF);
+
+  // Article Tags
+  static const Color poverty = Color(0xffEFCA58);
+  static const Color water = Color(0xff70C2DF);
+  static const Color pollution = Color(0xff509BE1);
+  static const Color india = Color(0xffFF71B2);
+}
